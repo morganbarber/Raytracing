2439f5b6f8fcbbc608610838a738e91ac86d4bd5
Morgan Barber <109038897+morganbarber@users.noreply.github.com>
2023-12-18 17:46:56 +0000

    Refactor hittable_list class
    The hittable_list class has been refactored to use std::shared_ptr instead of shared_ptr. The add() and clear() methods have also been modified.

--- src/hittable_list.hpp
+++ src/hittable_list.hpp
@@ -2,29 +2,32 @@
 #define HITTABLE_LIST_H
 
 #include "rtweekend.h"
-
 #include "hittable.h"
 
 #include <memory>
 #include <vector>
 
-
 class hittable_list : public hittable {
-  public:
-    std::vector<shared_ptr<hittable>> objects;
+public:
+    std::vector<std::shared_ptr<hittable>> objects;
 
     hittable_list() {}
-    hittable_list(shared_ptr<hittable> object) { add(object); }
 
-    void clear() { objects.clear(); }
+    hittable_list(std::shared_ptr<hittable> object) {
+        add(object);
+    }
 
-    void add(shared_ptr<hittable> object) {
+    void clear() {
+        objects.clear();
+    }
+
+    void add(std::shared_ptr<hittable> object) {
         objects.push_back(object);
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         hit_record temp_rec;
-        auto hit_anything = false;
+        bool hit_anything = false;
         auto closest_so_far = ray_t.max;
 
         for (const auto& object : objects) {
@@ -38,6 +41,5 @@ class hittable_list : public hittable {
         return hit_anything;
     }
 };
-
 
 #endif
